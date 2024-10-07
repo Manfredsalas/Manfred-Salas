ee441f4389572f774e95c269edd39bc236672143
Manfredsalas <manyaso06@outlook.com>
2024-10-07 14:21:09 -0600

    list comprehension

---
+++ List Comprehensions.py
@@ -0,0 +1,6 @@
+numberlist= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+odd_even = ["even" if num%2 == 0 else "odd" for num in numberlist]
+print(odd_even)
+
+three =[num for num in range (1,11) if num%3 == 0]
+print(three)
